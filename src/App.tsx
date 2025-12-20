176552a2f3d0d7f8390a9e8842bcdb1f3d04aeda
mikael <mikaeljuliao56@gmail.com>
2025-12-20 18:12:38 -0300

    chore: initial setup with Vite, React, TS and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">
+        Tailwind v3 funcionando
+      </h1>
+    </div>
+  )
+}
+
+export default App
